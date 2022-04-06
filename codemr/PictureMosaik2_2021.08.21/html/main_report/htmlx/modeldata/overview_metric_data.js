f37e17bb30c563aaa0daab24fa500bc0bc2ec487
Philipp Bleimund <maginir.philipp@gmail.com>
2022-04-06 17:20:02 +0200

    cleanup

--- codemr/PictureMosaik2_2021.08.21/html/main_report/htmlx/modeldata/overview_metric_data.js
+++
@@ -1,1 +0,0 @@
-var CodeMR_ProjectMetrics = {"Project Name":"PictureMosaik2", "Number of Highly Problematic Classes":0, "Number of Entities":34, "Number of Problematic Classes":0, "Class Lines of Code":1430, "Number of External Packages":43, "Number of Packages":5, "Number of External Entities":264}
--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"Computation"},{"l":"GUI"},{"l":"ImageViewer"},{"l":"PictureAnalyse"},{"l":"saveObjects"}]
